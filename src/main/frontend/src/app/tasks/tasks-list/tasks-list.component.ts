a6848389466b8c0c3b920d18c4260b47ac675652
maxproof <max.karberg@gmail.com>
2018-01-04 23:58:50 +0100

    Add static data (+ create before add and list)

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,30 @@
 import {Component, OnInit} from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.component.html',
   styleUrls: ['./tasks-list.component.css']
 })
+
 export class TasksListComponent implements OnInit {
+
+  tasks: Task[] = [];
 
   constructor() { }
 
   ngOnInit() {
+      this.tasks.push(new Task(1, "Task 1", true, "07/08/17"));
+      this.tasks.push(new Task(2, "Task 2", false, "07/08/17"));
+      this.tasks.push(new Task(3, "Task 3", false, "07/08/17"));
+  }
+
+  getDueDateLabel(task: Task) {
+      return task.completed ? 'label-success' : 'label-primary';
+  }
+
+  onTaskChange(event, task) {
+      console.log('Task has changed');
   }
 
 }
