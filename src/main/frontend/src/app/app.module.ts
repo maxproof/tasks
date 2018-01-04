da6c643dff76a3aa9702cdb483446e094583e1ee
maxproof <max.karberg@gmail.com>
2018-01-05 00:33:40 +0100

    On localhost:4200

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -6,6 +6,8 @@ import {AppComponent} from './app.component';
 import {TasksComponent} from './tasks/tasks.component';
 import {TasksAddComponent} from './tasks/tasks-add/tasks-add.component';
 import {TasksListComponent} from './tasks/tasks-list/tasks-list.component';
+import {HttpModule} from "@angular/http";
+import {TaskService} from "./tasks/task.service";
 
 
 @NgModule({
@@ -16,9 +18,10 @@ import {TasksListComponent} from './tasks/tasks-list/tasks-list.component';
     TasksListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
