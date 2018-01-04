33efcfa923c332564e34d444a3539cd63284ed96
maxproof <max.karberg@gmail.com>
2018-01-04 18:47:00 +0100

    Basic structure (controller, service, repo, domain)

--- src/main/java/com/therealdanvega/domain/Task.java
+++ src/main/java/com/therealdanvega/domain/Task.java
@@ -1,4 +1,25 @@
 package com.therealdanvega.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private LocalDate dueDate;
+    private Boolean completed;
 }
